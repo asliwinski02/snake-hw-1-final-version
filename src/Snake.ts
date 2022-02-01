bb202eac7b6908491ebe90721f3b9ac542a4256c
asliwinski02 <91695041+asliwinski02@users.noreply.github.com>
2022-01-31 19:57:27 -0500

    Initial commit

---
+++ src/Snake.ts
@@ -0,0 +1,26 @@
+import display from "./display";
+
+// place your code on
+
+public class Snake {}
+private String currentPosition: position;
+private String currentDirection: direction;
+
+move() {
+if Snake= "Facing Foward"
+ currentPosition="Increase its position by the given number"
+ else "Decrease its position by the given number"
+
+
+}
+turn () {
+  if Snake="Facing foward"
+  currentDirection="Turn it backward"
+  else"Turn it foward
+}
+public get position() {
+  return currentPosition
+}
+
+
+export default Snake;
